7a9958732518e4a743f348389c875824ab653cf4
Tobi <zcbtoal@ucl.ac.uk>
2017-11-29 02:40:22 +0100

    Alpha 0.2.1
    Selenium can access Absalon, find the desired elements for downloading,
    and then list their file names. Bug where using Chrome driver means only
    elements in page directly visible can be found, may switch to Firefox
    driver.

--- DownloadSlides.py
+++ DownloadSlides.py
@@ -18,7 +18,8 @@ def importcheck():
 
 
 try:
-    import placeholdermodulename
+    pass
+    #import placeholdermodulename
 except ImportError:
     importcheck()
 
@@ -28,14 +29,20 @@ from selenium.webdriver.common.keys import Keys
 
 
 absalon_url = "https://absalon.ku.dk"
+stats_course_files_url = "https://absalon.ku.dk/courses/22297/files?"
 print("Please provide your absalon username:")
 absalon_username = input()
 print("Please provide your absalon password")
 absalon_password = input()
 
-
-driver = webdriver.Chrome()
+# Launch a Chrome browser, access the absalon URL
+options = webdriver.ChromeOptions()
+options.add_argument('--window-size=500,500')
+driver = webdriver.Chrome(chrome_options=options)
+#driver.set_window_size(10, 10)
 driver.get(absalon_url)
+
+#Login to Absalon with provided username and password
 login_elem = driver.find_element_by_name("username")
 login_elem.clear()
 login_elem.send_keys(absalon_username)
@@ -44,3 +51,16 @@ password_elem.clear()
 password_elem.send_keys(absalon_password)
 password_elem.send_keys(Keys.RETURN)
 
+#Access a specific course url's files (in this example, Statistics for Bioinformatics and eScience)
+driver.get(stats_course_files_url)
+all_elements = driver.find_elements_by_class_name("ef-name-col__text")
+for element in all_elements:
+    print(element.text)
+
+#Check current working directory for presence of the files, if absent then downloads them to working directory
+#Might output the list to a file and so unwanted files aren't repeatedly downloaded
+
+
+
+#Finally close browser
+driver.close()
