14e7063690ab67786650abad0c9261cdbed91a4a
Tobi <zcbtoal@ucl.ac.uk>
2017-12-11 01:45:08 +0100

    Alpha 0.2.2
    Mixed things around a bit and modularised my importcheck function. Still
    unable to get geckodriver to work

--- DownloadSlides.py
+++ DownloadSlides.py
@@ -1,48 +1,36 @@
-#First must check if all packages needed are installed
-def importcheck():
-    print("Module not found, would you like to install it? Y/N")
-    for i in range(5):
-        install_response = input()
-        if install_response == "Y":
-            #install module
-            break
-        elif install_response == "N":
-            print("Cannot proceed without required modules")
-            quit()
-        elif i == 4:
-            print("Invalid input has been given ",i+1," times")
-            quit()
-        else:
-            print("Invalid input, try again. Would you like to install the missing module Y/N?")
-            continue
+#from CheckModules import importcheck
 
-
-try:
-    pass
-    #import placeholdermodulename
-except ImportError:
-    importcheck()
-
-#Attempt to login to Absalon/KU intranet
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-
-
+# Initiates constant variables
 absalon_url = "https://absalon.ku.dk"
 stats_course_files_url = "https://absalon.ku.dk/courses/22297/files?"
+which_browser = "Chrome"
+
+# Perform importcheck for required modules
+
+# Imports necessary modules/functions
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import os
+
+# Initiates user input variables - login details
 print("Please provide your absalon username:")
 absalon_username = input()
 print("Please provide your absalon password")
 absalon_password = input()
+print("What is your desired download directory")
+download_directory = input()
+
+# Launch a Firefox browser
+if which_browser == "Firefox":
+    driver = webdriver.Firefox()
 
 # Launch a Chrome browser, access the absalon URL
-options = webdriver.ChromeOptions()
-options.add_argument('--window-size=500,500')
-driver = webdriver.Chrome(chrome_options=options)
-#driver.set_window_size(10, 10)
-driver.get(absalon_url)
+if which_browser == "Chrome":
+    driver = webdriver.Chrome()
 
-#Login to Absalon with provided username and password
+# Access Absalon url and login to Absalon with provided username and password
+driver.get(absalon_url)
 login_elem = driver.find_element_by_name("username")
 login_elem.clear()
 login_elem.send_keys(absalon_username)
@@ -53,9 +41,20 @@ password_elem.send_keys(Keys.RETURN)
 
 #Access a specific course url's files (in this example, Statistics for Bioinformatics and eScience)
 driver.get(stats_course_files_url)
+time.sleep(10)
 all_elements = driver.find_elements_by_class_name("ef-name-col__text")
+os.chdir(download_directory)
+files_in_dir = os.listdir('.')
+print(files_in_dir)
 for element in all_elements:
     print(element.text)
+    if element in files_in_dir:
+        continue
+    else:
+        print("Getting this element")
+        file_address = element.get_attribute("src")
+        driver.get(file_address)
+
 
 #Check current working directory for presence of the files, if absent then downloads them to working directory
 #Might output the list to a file and so unwanted files aren't repeatedly downloaded
@@ -63,4 +62,4 @@ for element in all_elements:
 
 
 #Finally close browser
-driver.close()
+#driver.close()
